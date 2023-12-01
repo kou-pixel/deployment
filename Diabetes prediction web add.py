7eeb3ab944a1b780abeaf36d9c26e6f8c8cbc419
kou-pixel <125144263+kou-pixel@users.noreply.github.com>
2023-12-01 21:59:18 +0530

    Add files via upload

---
+++ Diabetes prediction web add.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  1 21:19:17 2023
+
+@author: USER
+"""
+
+import numpy as np
+import pickle
+import streamlit as st
+
+
+# loading the saved model
+loaded_model = pickle.load(open(r"C:\Users\USER\Downloads\trained_model.sav", 'rb'))
+
+
+
+  
+    
+  
